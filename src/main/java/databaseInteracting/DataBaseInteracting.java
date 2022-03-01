58cb6865f99ae0d72d9da6609c3dfb605ccf4008
Gabriel Escobar <gabrielgbg@gmail.com>
2022-03-01 12:35:13 -0300

    Correct the query to avoid duplicated results

--- src/main/java/databaseInteracting/DataBaseInteracting.java
+++ src/main/java/databaseInteracting/DataBaseInteracting.java
@@ -101,7 +101,7 @@ public class DataBaseInteracting {
   */
   public ResultSet searchForAllProductsOnShoppingCart(int cartId) throws SQLException {
     String sql = "SELECT p.id, p.name, p.price, l.quantity FROM products p," +
-        " shoppinglist l, shoppingcart c where p.id = l.idproduct AND l.idcart = ?;";
+        " shoppinglist l where p.id = l.idproduct AND l.idcart = ?;";
       PreparedStatement preparedStatement = connection.prepareStatement(sql);
       preparedStatement.setInt(1,cartId);
       return preparedStatement.executeQuery();
@@ -117,7 +117,7 @@ public class DataBaseInteracting {
   public ResultSet calculateTotalAmount(int shoppingCartId) throws SQLException {
     PreparedStatement preparedStatement = connection.prepareStatement(
         "SELECT SUM (p.price * l.quantity) AS totalSum FROM  products p," +
-            " shoppinglist l, shoppingcart c WHERE l.idProduct = p.id AND c.idShoppingCart = ?;");
+        " shoppinglist l, shoppingcart c WHERE l.idProduct = p.id AND c.idShoppingCart = ?;");
     preparedStatement.setInt(1, shoppingCartId);
     return preparedStatement.executeQuery();
   }
